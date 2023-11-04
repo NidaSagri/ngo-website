02dc7b602bde5b4161ebdeb1e5a787fc0118eec1
Nida Sagri <nidasagri@gmail.com>
2023-11-04 23:41:01 +0530

    added media query

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,9 @@ const Navbar=()=> {
    <div className="container-fluid">
     <Link className="navbar-brand logo" to="/"><img  src={logo} alt="NayePankh" /></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon">   
+    <i class="fas fa-bars" style={{color:"#fff"}}></i>
+</span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
